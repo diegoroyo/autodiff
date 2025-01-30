f398b86c236812711599637559ec25e333e43659
diegoroyo <diego.roymen@gmail.com>
2025-01-30 11:48:12 +0100

    wip: ...and works for andgate.cpp too

--- examples/andgate.cpp
+++ examples/andgate.cpp
@@ -3,7 +3,7 @@
 #include "autodiff/autodiff.h"
 
 int main() {
-    ad::Matrix<1, 2> w({2, 2});
+    ad::Matrix<1, 2> w({{2, 2}});
     ad::Value b(0);
 
     std::vector<std::array<float, 3>> samples = {
@@ -18,7 +18,7 @@ int main() {
     for (int i = 0; i < 20; ++i) {
         for (const auto& sample : samples) {
             auto [x0, x1, y] = sample;
-            common::Vec2f x(x0, x1);
+            common::Tensor<float, 2> x({x0, x1});
 
             auto y_est = ad::relu(w * x + b);
             auto loss = ad::pow(y_est - y, 2);
@@ -28,11 +28,11 @@ int main() {
         }
     }
 
-    std::cout << "w: " << w << "b: " << b << std::endl;
+    std::cout << "w: " << w << " b: " << b << std::endl;
 
     for (const auto& sample : samples) {
         auto [x0, x1, y] = sample;
-        common::Vec2f x(x0, x1);
+        common::Tensor<float, 2> x({x0, x1});
 
         auto y_est = ad::relu(w * x + b);
         std::cout << "x " << x << " y " << y << " y_est " << y_est.value()
